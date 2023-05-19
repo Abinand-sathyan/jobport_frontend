efcba4deee3a1c3bdf678854025d83f78013674b
abinand <abinandsathyan532gmail.com>
2023-05-19 18:54:25 +0530

    backend url changed

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://localhost:3001/',
+    baseURL:'https://jobport.onrender.com',
+    // baseURL:'http://localhost:3001/',
     headers: {
         "Content-Type": "application/json",
       }
